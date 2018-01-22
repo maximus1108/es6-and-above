e827866553b96d05aee88fe60aa1749821b9bbf8
Max Perry <max.perry1108@yahoo.co.uk>
2018-01-22 22:51:10 +0000

    more destructuring practise

--- src/destructuring.js
+++ src/destructuring.js
@@ -1,3 +1,5 @@
+document.write("<br><h1>Destructuring</h1>")
+
 {
     function bar() {
 		return {
@@ -12,5 +14,68 @@
 
 	console.log(a, b, c)
 	console.log(x, y, z);
+
+	document.write(`objects can be destructured to different reference names - a = ${a}; b = ${b}; c = ${c};<br>`)
+
+
+}
+
+{
+	function bar() {
+		return {
+			e: 4,
+			f: 5,
+			g: 6
+		};
+	}
+	
+	function foo() {
+		return [1, 2, 3];
+	}
+
+	let e, f, g, h , i , j;
+
+	({ e, f, g } = bar()); //wrapped in brackets as curleys will be considered as a block
+
+	[h, i, j] = foo();
+	
+	console.log(e,f,g);
+	console.log(h,i,j);
+
+	document.write(`variables can be declared before destructing<br>`)
+	
+} 
+
+{
+	function bar() {
+		return {
+			e: 4,
+			f: 5,
+			g: 6
+		};
+	}
+	
+	function foo() {
+		return [1, 2, 3];
+	}
+
+	const o = {}, arr = [];
+
+	[o.a, o.b, o.c] = foo();
+	({e: o.x, f: o.y, g: o.z } = bar()); //wrapped in brackets as curleys will be considered as a block
+
+	[arr[0], arr[1], arr[2]] = foo();
+	( { e: arr[3], f: arr[4], g: arr[5] } = bar() );
+
+	const prop = "name", letter = "g", obj = {};
+	
+
+
+	( { f: obj[prop], [letter]: obj[letter]  } = bar() )
+
+	console.log(o, arr, obj);
+
+	document.write(`variables previously declared can be objects or arrays which destructuring can be assigned to - ${arr}`)
+	document.write(`when destructuring, values can be assigned to an object property evaluated with [ "propname" ] brackets like usual assigment `)
 
 }
